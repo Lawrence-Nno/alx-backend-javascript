952dc95fe1fc95e8783cfd8db660fdc9a6c73033
Lawrence Nno <lawrence.nno@gmail.com>
2023-06-27 15:24:46 +0100

    Task 9

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,10 @@
+// A function for division
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+
+  const result = numerator / denominator;
+
+  return result;
+}
