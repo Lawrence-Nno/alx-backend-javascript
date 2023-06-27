0e0e213ff59ba955056cc8d2bbae5b144b8d049f
Lawrence Nno <lawrence.nno@gmail.com>
2023-06-27 16:06:34 +0100

    Removed extra spaces

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,13 @@
 // This function handles multiple promises
+
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName='', lastName='', fileName='') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then(() => {
+  }).catch(() => {
+  }).finally((values) => values);
 }
