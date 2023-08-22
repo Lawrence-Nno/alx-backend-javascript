c1f3829d4a9e2404aa9b58432e7d8f2e628bbee5
Lawrence Nno <lawrence.nno@gmail.com>
2023-08-22 10:15:12 +0100

    Task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,24 +4,29 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-app.get('/students', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/students', ((req, res) => {
   countStudents(process.argv[2])
-    .then((data) => {
-      res.end(data);
+    .then((v) => {
+      res.writeHead(200);
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${v.counter}\n`);
+      let len = 0;
+      for (const key in v.inFields) {
+        if (Object.prototype.hasOwnProperty.call(v.inFields, key)) {
+          res.write(`Number of students in ${key}: ${v.inFields[key].counter}. List: ${v.inFields[key].students}`);
+          if (len !== Object.keys(v.inFields).length - 1) res.write('\n');
+          len += 1;
+        }
+      }
+      res.end();
     })
-    .catch((error) => {
-      res.end(error.message);
+    .catch(() => {
+      res.write('This is the list of our students\n');
+      res.status(404).end('Cannot load the database');
     });
-});
+}));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port);
 
 module.exports = app;
